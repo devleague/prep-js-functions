8bd0bea52639a848fe383c3f02de0ed834ac793b
kwhitejr <kwhitejr.edu@gmail.com>
2016-11-21 18:58:44 -1000

    updated exercises and readme to reflect current teaching methods

--- functions.js
+++ functions.js
@@ -3,124 +3,122 @@
  *
  *  #1
  *  Declare Two Variables
- *     - `a` random Number value
- *     - `b` random Number value
+ *    @variable Datatype: Number `a`
+ *    @variable Datatype: Number `b`
  *
- *  We will be using both of these variables to pass as `parameters` to the
- *  following functions that we will write. Pay close attention to the other
+ *  These two variables will be passed as parameters to the
+ *  functions that we will write. Parameters are the function's arguments.
+ *  Parameters allow us to reuse a function with different values. 
+ *  Pay close attention to the other
  *  variable names you will create as they will become input to _other_
  *  functions.
  */
-var a = 13;
-var b = 29;
-// console.log(a+b);
+
 
 /**
  *  #2
  *  Function - add
  *
- *  This function returns the result of adding `a` and `b` together.
- *  Store this value in a variable named `sum`.
+ *    @param Datatype: Number `num1`
+ *    @param Datatype: Number `num2`
+ *    @return Datatype: Number
+ *
+ * This function takes two number arguments, `num1` and `num2`.
+ * This function `returns` back a number that is the sum of `num1` 
+ * and `num2`.
+ *
+ * You can test your function by invoking it with two arguments   
+ * and saving its return value to a variable. Store the return value
+ * in a variable named `sum`. Try invoking it with 
+ * your previously declared variables!
  */
-function add(num1, num2) {
-  return num1+num2;
-}
-var sum = add(24, 23);
-// console.log(sum);
+
 
 /**
  *  #3
  *  Function - subtract
  *
- *  This function returns the result of subtracting `b` from `a`.
- *  Store this value in a variable named `difference`
+ *    @param Datatype: Number `num1`
+ *    @param Datatype: Number `num2`
+ *    @return Datatype: Number
+ *
+ * This function takes two number arguments, `num1` and `num2`.
+ * This function `returns` back a number that is equal to `num2`  
+ * minus `num1`. Store the return value in a variable named 
+ * `difference`.
+ *
+ * Note that you can reuse the same parameter names without issue!
  */
 
-function subtract(num1, num2) {
-  return num2-num1;    
-}
-var difference = subtract(124, 283);
-// console.log(difference);
+
 /**
  *  #4
  *  Function - multiply
  *
- *  This function returns the result of multiplying `b` by `a`.
- *  Store this value in a variable named `product`
+ *    @param Datatype: Number `num1`
+ *    @param Datatype: Number `num2`
+ *    @return Datatype: Number
+ *
+ *  This function returns the result of multiplying `num1` by `num2`.
+ *  Store the return value in a variable named `product`.
  */
-function multiply(num1, num2) {
-  // var product = num1 * num2;
-  // return product;
-  return num2 * num1;
-}
-var product = multiply(5, 10);
-// console.log(product); // 50
+
 
 /**
  *  #5
  *  Function - checkDifference
  *
- *  This function accesses the value stored in the `difference`
- *  variable and uses this number to return the string
+ *    @param Datatype: Number `X`
+ *    @return Datatype: String
+ *
+ *  This function returns the string
  *  "My football team lost X times this week", where `X` is the
- *  value stored in `difference`.
+ *  value of the parameter.
+ *  Invoke this function using the variable `difference` as 
+ *  the parameter.
  */
 
-function checkDifference(X) {
-  return "My football team lost " + X + " times this week.";
-}
-var checkMinus = checkDifference(difference);
-// console.log(checkMinus); // My football team lost ... times this week.
 
 /**
  *  #6
  *  Function - checkSum
  *
- *  This function checks the value stored at `sum` and
- *  uses that number to print to the screen the phrase
+ *    @param Datatype: Number `X`
+ *    @return Datatype: String
+ *
+ *  This function returns the string
  *  "I CAN ADDZ X NUMBERS"  where `X` is the value
- *  stored in the variable `sum`.
+ *  of the parameter.
+ *  Invoke this function using the variable `sum` as 
+ *  the parameter.
  */
-function checkSum(X) {
-  return "I CAN ADDZ " + X + " NUMBERS";
-}
-var text2 = checkSum(sum);
-// console.log(text2); // I CAN ADDZ ... NUMBERS
+
 
 /**
  *  #7
  *  Function - checkProduct
  *
+ *    @return Datatype: Number
+ *
+ *  This function does not require any parameters.
  *  This function checks the value stored at `product` and
  *  multiplies it by the number stored at `difference` and
  *  then prints the result to the console.
+ *
+ *  Can you see how to use a previously-created function to help?
  */
 
-// function checkProduct() {
-//   console.log(product * difference);
-// }
-// checkProduct();
-
-
-// function checkProduct(asdfgadfasdf) {
-//   console.log("inside function", asdfgadfasdf);
-// }
-// var x = checkProduct(product, difference);
-// console.log("product", product);
-// console.log("difference", difference);
-// console.log("outside function", x);
-
-function checkProduct(p, d) {
-  return p*d;
-}
-var newProduct = checkProduct(7, multiply(3,5));
-// console.log(newProduct);
 
 /**
  *  #8
  *  Function - addThenSubtract
  *
- *  This function takes three `Number` arguments named by you,
+ *    @param Datatype: Number `numA`
+ *    @param Datatype: Number `numB`
+ *    @param Datatype: Number `numC`
+ *    @return Datatype: Number
+ *
+ *  This function takes three `Number` arguments,
  *  then adds the first two arguments together.
  *  Then with the sum of that operation, subtract
  *  the value at the third argument. _This function should
@@ -129,74 +127,56 @@ var newProduct = checkProduct(7, multiply(3,5));
  *  **example:**
  *      addThenSubtract(4, 5, 7); //-> returns 2 because 4 + 5 - 7 = 2
  */
-function addThenSubtract(num1, num2, num3) {
-  // method 1
-  return subtract(num3, add(num1, num2));
 
-  // method 2
-  // var result = subtract(num3, add(num1, num2));
-  // return result;
-
-  // method 3
-  // var first = add(num1,num2); // num1 + num2
-  // var second = subtract(num3, first); // first - num3
-  // return second;
-}
-
-var pumpkinPie = addThenSubtract(10, 5, 2);
-
-// console.log(pumpkinPie);
 
 /**
  *  #9
  *  Function - addThenMultiply
  *
- *  This function takes three `Number` arguments named by you,
+ *    @param Datatype: Number `numX`
+ *    @param Datatype: Number `numY`
+ *    @param Datatype: Number `numZ`
+ *    @return Datatype: Number
+ *
+ *  This function takes three `Number` arguments,
  *  then adds the first two arguments together.
  *  Then with the sum of that operation multiply it by the third argument.
  *  _This function also should make use of your previous functions_.
  *
- *  Store the return of this function to a variable named `howMany`
+ *  Store the return of this function to a variable named `howMany`.
  */
-function addThenMultiply(num1, num2, num3) {
-  return add(num1, num2)*num3;
-}
-var test = addThenMultiply(sum,difference,product);
-// console.log(test);
+
+
 /**
  *  #10
  *  Function - createFullName
  *
- *  @param Datatype: String firstName
- *  @param Datatype: String lastName
- *  @return Datatype: String
+ *    @param Datatype: String `firstName`
+ *    @param Datatype: String `lastName`
+ *    @return Datatype: String
  *
- * This function takes two String arguments `firstName` and `lastName`.
- * This function `returns` back a string which represents someone's full name.
+ *  This function takes two String arguments, `firstName` and `lastName`.
+ *  This function `returns` back a string which represents someone's full name.
  *
- * Call this function and pass your first and last name into it.
- * Store the return value to a variable named `myFullName`
+ *  **Call this function and pass your first and last name into it.
+ *  Store the return value to a variable named** `myFullName`.
  */
-function createFullName(firstName, lastName) {
-  var fullName = firstName + " " + lastName;
-  return fullName;
-}
-var myFullName = createFullName("Kevin", "White");
-console.log(myFullName);
+
 
 /**
  *  #11
  *  Function - verifyDrinkingAge
  *
- *  @param Datatype: Number age
+ *  @param Datatype: Number `age`
  *  @return Datatype: Boolean
  *
- *  This function takes one Number argument `age`.
- *  This function returns the `Boolean` value `true` or `false` if `age` is
- *  lower than the legal drinking age in the state of Hawaii.
+ *  This function takes one Number argument, `age`.
+ *  This function returns the `Boolean` value `true` or `false` depending
+ *  on whether `age` is lower than the legal drinking age
+ *  in the state of Hawaii.
  *
  *  **Call this function and pass in a number value.
- *  Store the return value to a variable named** `canDrinkBeer`
+ *  Store the return value to a variable named** `canDrinkBeer`.
  */
 
 
@@ -204,16 +184,24 @@ console.log(myFullName);
  *  #12
  *  Function - throwParty
  *
- *  This function checks the value stored at the `canDrinkBeer` variable and
- *  if the value is `false` it should print to a message to the screen,
- *  "The Party will have tons of Cake!" otherwise this message
- *  should be "This Party will have an open bar".
+ *  @return Datatype: Boolean
+ *
+ *  This function checks the value stored at the `canDrinkBeer` variable.
+ *  If the value is `false`, then it should print to a message to the screen:
+ *  "The Party will have tons of Cake!" 
+ * If not, then it should print a different message:
+ *  "This Party will have an open bar".
  */
 
 
 /**
  *  #13
  *  Function - eatFood
+ *
+ *    @param Datatype: String `firstName`
+ *    @param Datatype: String `lastName`
+ *    @param Datatype: String `food`
+ *    @return Datatype: String
  *
  *  This function takes 3 arguments
  *  `firstName`, `lastName`, `food` and prints out a message to your screen.
@@ -236,6 +224,8 @@ console.log(myFullName);
  *  #14
  *  Function - repeater
  *
+ *  @return Datatype: none
+
  *  This function will return a string to that repeats one of the phrases
  *  below `X` amount of times, where `X` is the value stored at `howMany`.
  *  Inside of this function it will check if
